b333f6a014d129fc886e358032432b500a296c4d
difuster <iam0kay@yandex.ru>
2023-04-24 20:39:37 +0600

    auth

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -24,7 +24,7 @@ const Header = ({ backLink = '' }) => {
 			) : (
 				<button
 					onClick={() => {
-						navigate('/profile');
+						navigate(isAuth ? '/profile' : '/auth');
 					}}
 				>
 					<SlUser className='header-icon' />
--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,8 +1,76 @@
 import React from 'react';
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import Loader from '../../components/ui/Loader'
+import Button from '../../components/ui/button/Button'
+import Field from '../../components/ui/field/Field'
 import Layout from '../../components/layouts/Layout';
+import styles from './Auth.module.scss';
+
+const isLoading = false;
+const isLoadingAuth = false;
 
 const Auth = () => {
-	return <Layout>Auth</Layout>;
+	const [type, setType] = useState('auth');
+
+	/* 
+	TODO:
+
+	[] - Auth context
+	[] - Axios
+	[] - React Query
+
+*/
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		mode: 'onChange'
+	});
+
+	const onSubmit = data => {
+		// type
+		console.log(data);
+	};
+
+	return (
+		<>
+			<Layout heading='Sign in' bgImage='/images/auth-bg.png' />
+			<div className='wrapper-inner-page'>
+				{(isLoading || isLoadingAuth) && <Loader />}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Field
+						error={errors?.email?.message}
+						name='email'
+						register={register}
+						options={{
+							required: 'Email is required'
+						}}
+						type='text'
+						placeholder='Enter email'
+					/>
+
+					<Field
+						error={errors?.password?.message}
+						name='password'
+						register={register}
+						options={{
+							required: 'Password is required'
+						}}
+						type='password'
+						placeholder='Enter password'
+					/>
+
+					<div className={styles.wrapperButtons}>
+						<Button clickHandler={() => setType('auth')}>Sign in</Button>
+						<Button clickHandler={() => setType('reg')}>Sign up</Button>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default Auth;
